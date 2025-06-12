497fcf3aa67fe1b2d48f78246221cf4172aa1d55
Anil <anil@test.com>
2025-06-12 23:44:58 +0530

    roboshop-kubernetes project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -6,9 +6,9 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   default_node_pool {
     name       = "default"
-    node_count = 2
-    vm_size    = "Standard_B2as_v2"
-    vnet_subnet_id = "/subscriptions/7d58fd8a-e8be-4f50-b9f6-03616700d9fc/resourceGroups/project-1/providers/Microsoft.Network/virtualNetworks/project-1-network/subnets/default"
+    node_count = var.default_node_pool["nodes"]
+    vm_size    = var.default_node_pool["vm_size"]
+    vnet_subnet_id = var.vnet_subnet_id
   }
 
   identity {
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   }
 
   aci_connector_linux {
-    subnet_name = "/subscriptions/7d58fd8a-e8be-4f50-b9f6-03616700d9fc/resourceGroups/project-1/providers/Microsoft.Network/virtualNetworks/project-1-network/subnets/default"
+    subnet_name = var.vnet_subnet_id
   }
 
   network_profile {
@@ -24,4 +24,15 @@ resource "azurerm_kubernetes_cluster" "main" {
     service_cidr = "10.100.0.0/24"
     dns_service_ip = "10.100.0.10"
   }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "main" {
+  for_each              = var.app_node_pool
+  name                  = each.key
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.main.id
+  vm_size               = each.value["vm_size"]
+  node_count            = each.value["min_count"]
+  min_count             = each.value["min_count"]
+  max_count             = each.value["max_count"]
+  auto_scaling_enabled  = each.value["auto_scaling_enabled"]
 }
